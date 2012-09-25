bd5d30ec510721b1143f504a3eb1c81309764d87
Jebastin Prabaharan <jebastin.prabaharan@photoninfotech.net>
2012-09-25 22:21:32 +0530

    [artf540002]Technology DAO storage and retrieval completed

---
+++ phresco-service/src/main/java/com/photon/phresco/service/dao/CustomerBaseDAO.java
@@ -0,0 +1,67 @@
+/*
+ * ###
+ * Phresco Service
+ * 
+ * Copyright (C) 1999 - 2012 Photon Infotech Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ###
+ */
+
+package com.photon.phresco.service.dao;
+
+import java.util.List;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+public class CustomerBaseDAO extends BaseDAO {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private List<String> customerIds;
+    private boolean used;
+	
+	public CustomerBaseDAO() {
+		super();
+	}
+
+    public List<String> getCustomerIds() {
+        return customerIds;
+    }
+
+    public void setCustomerIds(List<String> customerIds) {
+        this.customerIds = customerIds;
+    }
+    
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.DEFAULT_STYLE)
+                .append(super.toString())
+                .append("customerIds", getCustomerIds())
+                .append("isUsed", isUsed())
+                .toString();
+    }
+
+	
+}
