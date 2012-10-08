cc013cad081044e4899062aecd86bbd2221e161d
arunprasanna <arun.prasanna@photoninfotech.net>
2012-10-08 13:27:56 +0530

    [artf542682] Admin UI code review  - create separate package for SessionValidateInterceptor and role page alignment issue.

--- phresco-service-web/src/main/java/com/photon/phresco/service/admin/actions/interceptors/SessionValidatorInterceptor.java
+++ phresco-service-web/src/main/java/com/photon/phresco/service/admin/actions/interceptors/SessionValidatorInterceptor.java
@@ -1,4 +1,4 @@
-package com.photon.phresco.service.rest.interceptors;
+package com.photon.phresco.service.admin.actions.interceptors;
 
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.Interceptor;
